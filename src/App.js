8a0f85b8c1c46337e5f4cd89fc3768f0a0be0a09
Harish Sarika <79039916+Harish24-S@users.noreply.github.com>
2024-03-28 15:06:12 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import SkillSet from "./components/SkillSet";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <NavBar />
       <Home />
       <About />
-      <Portfolio />
+      <Projects />
       <Experience />
+      <SkillSet />
       <Contact />
 
       <SocialLinks />
